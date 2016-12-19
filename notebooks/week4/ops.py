720c6446cdd775d8f1b92e0e3eb6325a854574ba
yunjey <yunjey47@naver.com>
2016-12-19 23:04:16 +0900

    conv2d added

--- notebooks/week4/ops.py
+++ notebooks/week4/ops.py
@@ -1,6 +1,32 @@
 import tensorflow as tf
 
 
+def conv2d(x, channel_out, k_w=5, k_h=5, s_w=2, s_h=2, name=None):
+    """Computes Convolution Operation
+    
+    Args:
+        x: input tensor of shape (batch_size, width_in, heigth_in, channel_in)
+        channel_out: number of channel for output tensor
+        k_w: kernel width size; default is 5
+        k_h: kernel height size; default is 5
+        s_w: stride size for width; default is 2
+        s_h: stride size for heigth; default is 2
+        
+    Returns:
+        out: output tensor of shape (batch_size, width_out, height_out, channel_out)
+    """
+    channel_in = tf.shape(x)[-1]
+    
+    with tf.variable_scope(name):
+        w = tf.get_variable('w', shape=[k_w, k_h, channel_in, channel_out], 
+                            initializer=tf.truncated_normal_initializer(stddev=0.01))
+        b = tf.get_variable('b', shape=[channel_out], initializer=tf.constant_initializer(0.0))
+        
+        out = tf.nn.conv2d(x, w, strides=[1, s_w, s_h, 1], padding='SAME') + b
+        
+        return out
+    
+    
 def deconv2d(x, output_shape, k_w=5, k_h=5, s_w=2, s_h=2, name=None):
     """Computes Deconvolution Operation
     
@@ -15,7 +41,6 @@ def deconv2d(x, output_shape, k_w=5, k_h=5, s_w=2, s_h=2, name=None):
     Returns:
         out: output tensor of shape (batch_size, width_out, hegith_out, channel_out)
     """
-    
     channel_in = tf.shape(inputs)[-1]
     channel_out = tf.shape(output_shape)[-1]
     
