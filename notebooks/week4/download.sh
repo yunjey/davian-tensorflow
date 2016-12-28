55c7e7ec168fa7f5e5e50b87c876a01cd987b6db
yunjey <yunjey47@naver.com>
2016-12-29 00:48:51 +0900

    download celeb image dataset

--- notebooks/week4/download.sh
+++ notebooks/week4/download.sh
@@ -1,2 +1,3 @@
-wget https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADIKlz8PR9zr6Y20qbkunrba/Img/img_align_celeba.zip?dl=1&pv=1 -P data/
-unzip img_align_celeba.zip?dl=1 -d data/
+mkdir -p data
+wget -O data/img_align_celeba.zip "https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADIKlz8PR9zr6Y20qbkunrba/Img/img_align_celeba.zip?dl=1&pv=1"
+unzip data/img_align_celeba.zip -d data/
